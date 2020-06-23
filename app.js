624f727a892c3e63e938ff051435de41b0cdec0e
Jasper Caballero <jvicaballero@gmail.com>
2020-06-23 12:39:29 -0400

    All working backend changes

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const helmet = require("helmet");
 const compression = require("compression");
 
 // Utilities;
-//const createLocalDatabase = require("./utils/createLocalDatabase");
 const seedDatabase = require("./utils/seedDatabase");
 
 //NEW ADDITION
@@ -67,7 +66,7 @@ passport.deserializeUser(async (id, done) => {
 //     done(err);
 //   }
 // });
-// A helper function to sync our database;
+//A helper function to sync our database;
 const syncDatabase = () => {
   if (process.env.NODE_ENV === "production") {
     db.sync();
@@ -76,12 +75,6 @@ const syncDatabase = () => {
     db.sync({ force: true })
       .then(() => seedDatabase())
       .catch((err) => {
-        // if (err.name === "SequelizeConnectionError") {
-        //  // createLocalDatabase();
-        //   seedDatabase();
-        // } else {
-        //   console.log(err);
-        // }
         console.log(err);
       });
   }
@@ -148,82 +141,14 @@ const startListening = () => {
 
 const bootApp = async () => {
   await sessionStore.sync();
-  await syncDb();
   await syncDatabase();
   await configureApp();
   await startListening();
 };
 
-//end
-
-// Utilities;
-const createLocalDatabase = require("./utils/createLocalDatabase");
-
-// Our database instance;
-//const db = require("./database");
-
-// A helper function to sync our database;
-const syncDatabase = () => {
-  if (process.env.NODE_ENV === "production") {
-    db.sync();
-  } else {
-    console.log("As a reminder, the forced synchronization option is on");
-    db.sync({ force: true })
-      .then(() => seedDatabase())
-      .catch((err) => {
-        if (err.name === "SequelizeConnectionError") {
-          createLocalDatabase();
-          seedDatabase();
-        } else {
-          console.log(err);
-        }
-      });
-  }
-};
 
 // Instantiate our express application;
 const app = express();
-
-// A helper function to create our app with configurations and middleware;
-//const configureApp = () => {
-// app.use(helmet());
-// app.use(logger("dev"));
-// // handle request data:
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(compression());
-// app.use(cookieParser());
-
-// // Our apiRouter
-// const apiRouter = require("./routes/index");
-
-// // Mount our apiRouter
-// app.use("/api", apiRouter);
-
-// // Error handling;
-// app.use((req, res, next) => {
-//   if (path.extname(req.path).length) {
-//     const err = new Error("Not found");
-//     err.status = 404;
-//     next(err);
-//   } else {
-//     next();
-//   }
-// });
-
-// // More error handling;
-// app.use((err, req, res, next) => {
-//   console.error(err);
-//   console.error(err.stack);
-//   res.status(err.status || 500).send(err.message || "Internal server error.");
-// });
-//};
-
-// Main function declaration;
-// const bootApp = async () => {
-//   await syncDatabase();
-//   await configureApp();
-// };
 
 // Main function invocation;
 bootApp();
